e94d31e52abc17bec6cbb866667a4926bcc21c1c
b22013685 <rayane.belabbas@etu.univ-amu.com>
2023-12-11 15:15:49 +0100

    DEBUT HOME

---
+++ View/Home.php
@@ -0,0 +1,7 @@
+<?php
+
+require 'GestionPage.php';
+
+start_page('Acceuil');//Charge la balise "head" avec le css, favicon et le nom de la page donner en parametre.
+end_page();
+?>
--- index.php
+++ index.php
@@ -1,1 +1,2 @@
 <?php
+header('Location: View/Home.php');
