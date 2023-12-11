44dcea931a2fe7faf83a48658216ccf5b4a78588
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-12-11 11:03:58 +0100

    Création des pages règles

---
+++ View/Rules/rulesEquipment.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Règles Officielles - Hockey sur glace</title>
+        <!-- Ajoutez des liens vers vos fichiers CSS si nécessaire -->
+    </head>
+    <body>
+
+        <header>
+            <h1>Equipements</h1>
+        </header>
+
+        <section>
+            <p>L'équipement complet d'un joueur de Hockey sur glace se compose ainsi :
+                Une crosses
+                Des patins
+                L'équipement de protection (casque, coudières, jambières, gants, etc.)
+                L'uniforme de l'équipe
+
+                Le gardien possède des protections suplémentaire ainsi qu’un crosse différente.</p>
+        </section>
+
+        <section>
+            <p>Il existe 3 type de protection pour le casque :</p>
+        </section>
+
+        <footer>
+            <p>© 2023 Hockey sur glace</p>
+            <a href="index.php">Retour à l'accueil</a>
+            <a href="rulesGameSurface.php">Précédant</a>
+            <a href="rulesGame.php">Jeu</a>
+        </footer>
+
+    </body>
+</html>
---
+++ View/Rules/rulesOffSide.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Règles Officielles - Hockey sur glace</title>
+        <!-- Ajoutez des liens vers vos fichiers CSS si nécessaire -->
+    </head>
+    <body>
+
+        <header>
+            <h1>Le Hors Jeu</h1>
+        </header>
+
+        <section>
+            <p>Un joueur qui attaque n’a pas le droit de précéder le palet dans la zone d’attaque. C’est ce qu’on appelle le hors-jeu. Autre règle très importante, celle du dégagement interdit. On appelle un dégagement interdit quand le palet tiré depuis la demie défensive franchit la ligne de but adverse alors que les deux équipes sont à force égale ou que l’équipe qui attaque est en supériorité numérique.</p>
+        </section>
+
+        <footer>
+            <p>© 2023 Hockey sur glace</p>
+            <a href="index.php">Retour à l'accueil</a>
+            <a href="rulesGame.php">Précédant</a>
+        </footer>
+
+    </body>
+</html>
