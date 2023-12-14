2beacbd10af0dca421806e3285627501e33ccd04
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-12-14 19:47:56 +0100

    Ajout du css dans la page hors jeu des règles

--- view/rules/offSide.php
+++ view/rules/offSide.php
@@ -1,6 +1,23 @@
-<h1>Le Hors Jeu</h1>
-
-<p>Un joueur qui attaque n’a pas le droit de précéder le palet dans la zone d’attaque. C’est ce qu’on appelle le hors-jeu. Autre règle très importante, celle du dégagement interdit. On appelle un dégagement interdit quand le palet tiré depuis la demie défensive franchit la ligne de but adverse alors que les deux équipes sont à force égale ou que l’équipe qui attaque est en supériorité numérique.</p>
-
-<button onclick="window.location.href='/home'">Accueil</button>
-<button onclick="window.location.href='/rules/game'">Precedent</button>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Serious Game de hockey pour les Spartiate de Marseille"/>
+        <link rel="icon" href="/image/favicon.ico" type="image/x-icon" />
+        <title>Titre</title>
+        <link id="theme" rel="stylesheet" href="../Style/style.css" media="all and (orientation: landscape)"> <!--css pour ecran en paysage-->
+    </head>
+    <body>
+    <header>
+        <div class="text-container">
+            <h1>Le Hors Jeu</h1>
+        </div>
+    </header>
+    <section class="text-container">
+        <p>Un joueur qui attaque n’a pas le droit de précéder le palet dans la zone d’attaque. C’est ce qu’on appelle le hors-jeu.</p>
+        <p>Autre règle très importante, celle du dégagement interdit. On appelle un dégagement interdit quand le palet tiré depuis la demie défensive franchit la ligne de but adverse alors que les deux équipes sont à force égale ou que l’équipe qui attaque est en supériorité numérique.</p></p>
+    </section>
+    <button class="left" onclick="window.location.href='home'">Accueil</button>
+    <button class="right" onclick="window.location.href='rules&page=3'">Precedent</button>
+    </body>
+</html>
