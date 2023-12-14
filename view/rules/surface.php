67e910282f086b7d8c98675dd66911faf2ac9a46
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-12-14 19:50:16 +0100

    Ajout du css dans la page surface des règles

--- view/rules/surface.php
+++ view/rules/surface.php
@@ -1,13 +1,29 @@
-<h1>Surface de jeu</h1>
-
-<p>La patinoire est divisée en 3 zones, délimitées par 2 lignes bleues: la zone d’attaque, la zone neutre (au milieu) et la zone de défense.</p>
-<p>La zone de défense est comprise entre la balustrade derrière le but défendu par l’équipe et la première ligne bleue. Cette zone comprend également une ligne rouge fine, appelée ligne de but, sur laquelle est placé le but à défendre.</p>
-<p>La zone neutre, quant à elle, est la partie de la patinoire comprise entre les deux lignes bleues. Au milieu de la zone, une ligne rouge aussi épaisse que les lignes bleues symbolise le milieu de la glace et est appelée la ligne rouge centrale. </p>
-<p>Enfin, la zone d’attaque va de la deuxième ligne bleue à la
-    bande derrière le but défendu par l’équipe adverse.
-    Elle comprend également l’autre ligne de but de la patinoire,
-    sur laquelle est positionné le but adverse.</p>
-
-<button onclick="window.location.href='/home'">Accueil</button>
-<button onclick="window.location.href='/rules/rules'">Precedent</button>
-<button onclick="window.location.href='/rules/equipments'">Suivant</button>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Serious Game de hockey pour les Spartiate de Marseille"/>
+        <link rel="icon" href="/image/favicon.ico" type="image/x-icon" />
+        <title>Titre</title>
+        <link id="theme" rel="stylesheet" href="../Style/style.css" media="all and (orientation: landscape)"> <!--css pour ecran en paysage-->
+    </head>
+    <body>
+        <header>
+            <div class="text-container">
+                <h1>Surface de jeu</h1>
+            </div>
+        </header>
+        <section class="text-container">
+            <p>La patinoire est divisée en 3 zones, délimitées par 2 lignes bleues: la zone d’attaque, la zone neutre (au milieu) et la zone de défense.</p>
+            <p>La zone de défense est comprise entre la balustrade derrière le but défendu par l’équipe et la première ligne bleue. Cette zone comprend également une ligne rouge fine, appelée ligne de but, sur laquelle est placé le but à défendre.</p>
+            <p>La zone neutre, quant à elle, est la partie de la patinoire comprise entre les deux lignes bleues. Au milieu de la zone, une ligne rouge aussi épaisse que les lignes bleues symbolise le milieu de la glace et est appelée la ligne rouge centrale. </p>
+            <p>Enfin, la zone d’attaque va de la deuxième ligne bleue à la
+                bande derrière le but défendu par l’équipe adverse.
+                Elle comprend également l’autre ligne de but de la patinoire,
+                sur laquelle est positionné le but adverse.</p>
+        </section>
+        <button class="left" onclick="window.location.href='home'">Accueil</button>
+        <button onclick="window.location.href='rules&page=2'">Precedent</button>
+        <button class="right" onclick="window.location.href='rules&page=4'">Suivant</button>
+    </body>
+</html>
