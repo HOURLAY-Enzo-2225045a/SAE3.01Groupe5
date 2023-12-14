737ebf6ed10cf3823dd44fcad6e044f6602894d9
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-12-14 19:45:24 +0100

    Ajout du css dans la page equipement des règles

--- view/rules/equipments.php
+++ view/rules/equipments.php
@@ -1,15 +1,37 @@
-<h1>Equipements</h1>
-
-<p>L'équipement complet d'un joueur de Hockey sur glace se compose ainsi :
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Serious Game de hockey pour les Spartiate de Marseille"/>
+    <link rel="icon" href="/image/favicon.ico" type="image/x-icon" />
+    <title>Titre</title>
+    <link id="theme" rel="stylesheet" href="../Style/style.css" media="all and (orientation: landscape)"> <!--css pour ecran en paysage-->
+</head>
+    <body>
+        <header>
+            <div class="text-container">
+                <h1>Equipements</h1>
+            </div>
+        </header>
+        <article class="text-container">
+            <p>L'équipement complet d'un joueur de Hockey sur glace se compose ainsi :
+                <br>
                 - Une crosses
+                <br>
                 - Des patins
+                <br>
                 - L'équipement de protection (casque, coudières, jambières, gants, etc.)
+                <br>
                 - L'uniforme de l'équipe
-    Le gardien possède des protections suplémentaire ainsi qu’un crosse différente.
-
-    Il existe 3 type de protection pour le casque :
-</p>
-
-<button onclick="window.location.href='/home'">Accueil</button>
-<button onclick="window.location.href='/rules/surface'">Precedent</button>
-<button onclick="window.location.href='/rules/game'">Suivant</button>
+                <br>
+                Le gardien possède des protections suplémentaire ainsi qu’un crosse différente.
+            </p>
+            <p>
+                Il existe 3 type de protection pour le casque :
+            </p>
+        </article>
+        <button onclick="window.location.href='/home'">Accueil</button>
+        <button onclick="window.location.href='/rules/surface'">Precedent</button>
+        <button onclick="window.location.href='/rules/game'">Suivant</button>
+    </body>
+</html>
