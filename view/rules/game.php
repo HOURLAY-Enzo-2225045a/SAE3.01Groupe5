cfce29f1d03af89a1805f8dc4ac8b471461131f1
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-12-14 19:37:19 +0100

    Ajout du css dans la page game des règles

--- view/rules/game.php
+++ view/rules/game.php
@@ -1,17 +1,34 @@
-<h1>Jeu</h1>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Serious Game de hockey pour les Spartiate de Marseille"/>
+    <link rel="icon" href="/image/favicon.ico" type="image/x-icon" />
+    <title>Titre</title>
+    <link id="theme" rel="stylesheet" href="../Style/style.css" media="all and (orientation: landscape)"> <!--css pour ecran en paysage-->
+</head>
+<body>
+<header>
+    <div class="text-container">
+        <h1>Jeu</h1>
+    </div>
+</header>
 
-<p>Un match de hockey sur glace se déroule en 3 périodes de 15 à 20 minutes, selon les catégories d’âge, avec une pause de 15 minutes entre chacune d’entre elles.</p>
-<p>L’équipe qui marque le plus de buts au cours de la rencontre est déclarée vainqueur.
-<p>Certains matches peuvent être complétés d’une prolongation, en cas de match nul entre les deux équipes.</p>
-<p>Si celle-ci ne suffit pas à les départager, commence alors une séance de tirs au but.</p>
-<p>Pour gêner l’adversaire, les hockeyeurs ont le droit de rentrer en contact avec lui pour le déstabiliser et récupérer le palet.
-    En cas de geste interdit, un joueur peut être exclu de la glace pendant plusieurs minutes, laissant son équipe en infériorité numérique le temps de son exclusion.
-</p>
-<p>Dans un match, une équipe peut avoir au maximum 20 joueurs de champ + 2 gardiens</p>
-<p>Il y a forcément un capitaine par équipe + 2 capitaine assistant maximum 
-    Le capitaine porte un “C” et les assistant un “A” bien visible sur le devant du maillot pour les identifier.
-</p>
-
-<button onclick="window.location.href='/home'">Accueil</button>
-<button onclick="window.location.href='/rules/equipments'">Precedent</button>
-<button onclick="window.location.href='/rules/offSide'">Suivant</button>
+<section class="text-container">
+    <p>Un match de hockey sur glace se déroule en 3 périodes de 15 à 20 minutes, selon les catégories d’âge, avec une pause de 15 minutes entre chacune d’entre elles.</p>
+    <p>L’équipe qui marque le plus de buts au cours de la rencontre est déclarée vainqueur.
+    <p>Certains matches peuvent être complétés d’une prolongation, en cas de match nul entre les deux équipes.</p>
+    <p>Si celle-ci ne suffit pas à les départager, commence alors une séance de tirs au but.</p>
+    <p>Pour gêner l’adversaire, les hockeyeurs ont le droit de rentrer en contact avec lui pour le déstabiliser et récupérer le palet.
+        En cas de geste interdit, un joueur peut être exclu de la glace pendant plusieurs minutes, laissant son équipe en infériorité numérique le temps de son exclusion.
+    </p>
+    <p>Dans un match, une équipe peut avoir au maximum 20 joueurs de champ + 2 gardiens</p>
+    <p>Il y a forcément un capitaine par équipe + 2 capitaine assistant maximum 
+        Le capitaine porte un “C” et les assistant un “A” bien visible sur le devant du maillot pour les identifier.
+    </p>
+</section>
+<button class="left" onclick="window.location.href='home'">Accueil</button>
+<button onclick="window.location.href='rules&page=1'">Precedent</button>
+<button class="right" onclick="window.location.href='rules&page=3'">Suivant</button>
+</body>
+</html>
