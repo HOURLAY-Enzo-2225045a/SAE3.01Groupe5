de37eb66a6d5c6921ee12e1c906b1f3eada05256
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-12-14 19:49:07 +0100

    Ajout du css dans la page principal des règles

--- view/rules/rules.php
+++ view/rules/rules.php
@@ -1,12 +1,29 @@
-<h1>Les règles du jeu</h1>
-
-<p>Le hockey sur glace est un sport qui se pratique à 6 contre 6, avec 1 gardien et 5 joueurs par équipe.</p>
-<p>L’objectif du jeu est de marquer un maximum de buts en envoyant un disque en caoutchouc appelé palet, dans le but adverse.</p>
-<p>Pour manipuler le palet, les joueurs utilisent une crosse de hockey.</p>
-<p>Le hockey sur glace est le seul sport où les joueurs peuvent se déplacer derrière les buts !</p>
-<p>Le terrain de jeu, appelé patinoire, mesure entre 56 et 60 mètres de long, et entre 26 à 30 mètres de large.</p>
-<p>Il est entouré par des balustrades.</p>
-
-<button onclick="window.location.href='/home'">Accueil</button>
-<button onclick="window.location.href='/rules/surface'">Suivant</button>
-
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Serious Game de hockey pour les Spartiate de Marseille"/>
+        <link rel="icon" href="/image/favicon.ico" type="image/x-icon" />
+        <title>Titre</title>
+        <link id="theme" rel="stylesheet" href="../Style/style.css" media="all and (orientation: landscape)"> <!--css pour ecran en paysage-->
+    </head>
+    <body>
+    <header>
+        <div class="text-container">
+            <h1>Bienvenue "Username !"</h1>
+            <h2>Ici je vais t’apprendre les basiques du hockey sur glace.</h2>
+            <h2>Alors enfile tes patins et c’est partie !</h2>
+        </div>
+    </header>
+    <section class="text-container">
+        <p>Le hockey sur glace est un sport qui se pratique à <strong>6 contre 6</strong>, avec <strong>1 gardien</strong> et <strong>5 joueurs</strong> par équipe.</p>
+        <p>L’objectif du jeu est de marquer un maximum de buts en envoyant un disque en caoutchouc appelé <strong>palet</strong>, dans le but adverse.</p>
+        <p>Pour manipuler le palet, les joueurs utilisent une <strong>crosse de hockey</strong>.</p>
+        <p>Le hockey sur glace est le seul sport où les joueurs peuvent se déplacer derrière les buts !</p>
+        <p>Le terrain de jeu, appelé patinoire, mesure entre 56 et 60 mètres de long, et entre 26 à 30 mètres de large.</p>
+        <p>Il est entouré par des balustrades.</p>
+    </section>
+    <button class="left" onclick="window.location.href='home'">Accueil</button>
+    <button class="right" onclick="window.location.href='rules&page=1'">Suivant</button>
+    </body>
+</html>
