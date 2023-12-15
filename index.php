555a2eb2b3a81cfe6f3b56b906e4696c36100093
RayaneBELABBASB22013685 <rayanebelabbas15@gmail.com>
2023-12-15 11:51:53 +0100

    index

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ require_once 'Repository/AbstractRepository.php';
 require_once 'Repository/UserRepository.php';
 require_once 'Model/User.php';
 
+include_once 'Exception/CannotCreateException.php';
+include_once 'Exception/EmptyFieldException.php';
+include_once 'Exception/MoreThanOneException.php';
+include_once 'Exception/NotFoundException.php';
+include_once 'Exception/PasswordVerificationException.php';
+
 use App\Controller\SignUpController;
 
 header('Location: View/Home.php');
