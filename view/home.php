c6415897c5b19992b9f657e6f3affbc8d0b14e16
Alex28345 <114405664+Alex28345@users.noreply.github.com>
2023-12-15 13:48:55 +0100

    update gitignore

--- view/home.php
+++ view/home.php
@@ -6,7 +6,7 @@
     <button class="absolute top-4 right-4 bg-white w-1/4 md:w-1/5 py-1.5 drop-shadow-2xl text-lg" onclick="window.location.href='signUp'">CONNEXION</button>
 </div>
 
-<div class="w-full h-[50vh] flex flex-col justify-center items-center" >
+<div class="w-full h-[50vh] flex flex-col justify-center items-center">
     <button class="bg-white w-1/2 md:w-1/3 py-8 md:py-6 drop-shadow-xl text-lg my-4" onclick="window.location.href='play'">JOUER</button>
     <button class="bg-white w-1/2 md:w-1/3 py-8 md:py-6 drop-shadow-xl text-lg my-4" onclick="window.location.href='rules'">LES REGLES</button>
     <button class="bg-[var(--color-btn-admin)] w-1/3 py-4 drop-shadow-xl text-lg my-4 hidden" onclick="window.location.href='admin'">ADMIN</button>
