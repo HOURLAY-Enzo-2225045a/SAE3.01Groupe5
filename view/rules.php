45e9d3bcb4e0ac4fc196fc94f88a16ef13b74fb6
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-12-22 00:09:27 +0100

    Carrousel

---
+++ view/rules.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Votre description ici">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel/slick/slick-theme.css"/>
+
+    <title>Votre Titre</title>
+</head>
+<body class="font-sans bg-gray-100">
+
+<div class="carousel mx-auto bg-white p-4 shadow-lg">
+    <!-- Slide 1 -->
+    <div class="slide bg-gray-200 p-8 rounded-md text-center">
+        <!--<img src="image1.jpg" alt="Image 1" class="max-w-full mb-4">-->
+        <h1 class="text-2xl font-semibold text-gray-800 mb-2">Bienvenue "Username !"</h1>
+        <h2>Ici je vais t’apprendre les basiques du hockey sur glace.</h2>
+        <h2>Alors enfile tes patins et c’est partie !</h2>
+        <p class="text-gray-600 mb-4">Le hockey sur glace est un sport qui se pratique à <strong>6 contre 6</strong>, avec <strong>1 gardien</strong> et <strong>5 joueurs</strong> par équipe.</p>
+        <p class="text-gray-600 mb-4">L’objectif du jeu est de marquer un maximum de buts en envoyant un disque en caoutchouc appelé <strong>palet</strong>, dans le but adverse.</p>
+        <p class="text-gray-600 mb-4">Pour manipuler le palet, les joueurs utilisent une <strong>crosse de hockey</strong>.</p>
+        <p class="text-gray-600 mb-4">Le hockey sur glace est le seul sport où les joueurs peuvent se déplacer derrière les buts !</p>
+        <p class="text-gray-600 mb-4">Le terrain de jeu, appelé patinoire, mesure entre 56 et 60 mètres de long, et entre 26 à 30 mètres de large.</p>
+        <p class="text-gray-600 mb-4">Il est entouré par des balustrades.</p>
+        <!--<button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700">Bouton Slide 1</button>-->
+    </div>
+
+    <!-- Slide 2 -->
+    <div class="slide bg-gray-200 p-8 rounded-md text-center">
+        <h1 class="text-2xl font-semibold text-gray-800 mb-2">Surface de jeu</h1>
+        <p class="text-gray-600 mb-4">La patinoire est divisée en 3 zones, délimitées par 2 lignes bleues: la zone d’attaque, la zone neutre (au milieu) et la zone de défense.</p>
+        <p class="text-gray-600 mb-4">La zone de défense est comprise entre la balustrade derrière le but défendu par l’équipe et la première ligne bleue. Cette zone comprend également une ligne rouge fine, appelée ligne de but, sur laquelle est placé le but à défendre.</p>
+        <p class="text-gray-600 mb-4">La zone neutre, quant à elle, est la partie de la patinoire comprise entre les deux lignes bleues. Au milieu de la zone, une ligne rouge aussi épaisse que les lignes bleues symbolise le milieu de la glace et est appelée la ligne rouge centrale. </p>
+        <p class="text-gray-600 mb-4">Enfin, la zone d’attaque va de la deuxième ligne bleue à la
+            bande derrière le but défendu par l’équipe adverse.
+            Elle comprend également l’autre ligne de but de la patinoire,
+            sur laquelle est positionné le but adverse.</p>
+    </div>
+
+    <!-- Slide 3 -->
+    <div class="slide bg-gray-200 p-8 rounded-md text-center">
+        <h1 class="text-2xl font-semibold text-gray-800 mb-2">Equipements</h1>
+        <p class="text-gray-600 mb-4">L'équipement complet d'un joueur de Hockey sur glace se compose ainsi :
+            <br>
+            - Une crosses
+            <br>
+            - Des patins
+            <br>
+            - L'équipement de protection (casque, coudières, jambières, gants, etc.)
+            <br>
+            - L'uniforme de l'équipe
+            <br>
+            Le gardien possède des protections suplémentaire ainsi qu’un crosse différente.
+        </p>
+        <p class="text-gray-600 mb-4">
+            Il existe 3 type de protection pour le casque :
+        </p>
+    </div>
+
+    <!-- Slide 4 -->
+    <div class="slide bg-gray-200 p-8 rounded-md text-center">
+        <h1 class="text-2xl font-semibold text-gray-800 mb-2">Jeu</h1>
+        <p class="text-gray-600 mb-4">Un match de hockey sur glace se déroule en 3 périodes de 15 à 20 minutes, selon les catégories d’âge, avec une pause de 15 minutes entre chacune d’entre elles.</p>
+        <p class="text-gray-600 mb-4">L’équipe qui marque le plus de buts au cours de la rencontre est déclarée vainqueur.
+        <p class="text-gray-600 mb-4">Certains matches peuvent être complétés d’une prolongation, en cas de match nul entre les deux équipes.</p>
+        <p class="text-gray-600 mb-4">Si celle-ci ne suffit pas à les départager, commence alors une séance de tirs au but.</p>
+        <p class="text-gray-600 mb-4">Pour gêner l’adversaire, les hockeyeurs ont le droit de rentrer en contact avec lui pour le déstabiliser et récupérer le palet.
+            En cas de geste interdit, un joueur peut être exclu de la glace pendant plusieurs minutes, laissant son équipe en infériorité numérique le temps de son exclusion.
+        </p>
+        <p class="text-gray-600 mb-4">Dans un match, une équipe peut avoir au maximum 20 joueurs de champ + 2 gardiens</p>
+        <p class="text-gray-600 mb-4">Il y a forcément un capitaine par équipe + 2 capitaine assistant maximum 
+            Le capitaine porte un “C” et les assistant un “A” bien visible sur le devant du maillot pour les identifier.
+        </p>
+    </div>
+
+    <!-- Slide 5 -->
+    <div class="slide bg-gray-200 p-8 rounded-md text-center">
+        <h1 class="text-2xl font-semibold text-gray-800 mb-2">Le Hors Jeu</h1>
+        <p class="text-gray-600 mb-4">Un joueur qui attaque n’a pas le droit de précéder le palet dans la zone d’attaque. C’est ce qu’on appelle le hors-jeu.</p>
+        <p class="text-gray-600 mb-4">Autre règle très importante, celle du dégagement interdit. On appelle un dégagement interdit quand le palet tiré depuis la demie défensive franchit la ligne de but adverse alors que les deux équipes sont à force égale ou que l’équipe qui attaque est en supériorité numérique.</p>
+    </div>
+
+</div>
+
+<button id="prevSlideButton" class="absolute bottom-4 left-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700" onclick="prevSlide()">Précédent</button>
+<button id="nextSlideButton" class="absolute bottom-4 right-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700" onclick="nextSlide()">Suivant</button>
+
+</body>
+</html>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel/slick/slick.min.js"></script>
+<script>
+    $(document).ready(function(){
+        $('.carousel').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 5000,
+        });
+    });
+
+    function prevSlide() {
+        $('.carousel').slick('slickPrev');
+    }
+
+    function nextSlide() {
+        $('.carousel').slick('slickNext');
+    }
+</script>
