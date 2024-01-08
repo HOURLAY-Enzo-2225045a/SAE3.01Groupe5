e7e97322296c5596bc24673f65aa7bac2e18b925
Enzo HOURLAY <enzo.hourlay@etu.univ-amu.fr>
2024-01-08 18:11:43 +0100

    TestJS.php changement du end_page()

--- View/TestJS.php
+++ View/TestJS.php
@@ -4,4 +4,6 @@ require 'GestionPage.php';
 
 start_page('Test JS');   //Charge la balise "head" avec le css, favicon et le nom de la page donner en parametre.
 testJs_view("Test JS");
-end_page();
+?>
+    </body>
+</html>
