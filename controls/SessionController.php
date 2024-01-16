61d9cb8608838eca8cb59f80a0d7fb7ad84f3594
Alex28345 <114405664+Alex28345@users.noreply.github.com>
2024-01-16 13:17:22 +0100

    suppression user

--- controls/SessionController.php
+++ controls/SessionController.php
@@ -33,7 +33,7 @@ class SessionController
                 <td class="px-4 py-2 border-t border-b text-center">' . $sessionUser->getPseudo() .'</td>
                 <td class="px-4 py-2 border-t border-b text-center">' . $sessionUser->getScore().'</td>
                 <td class="p-2 border bg-[var(--color-bg)] text-center">
-                    <button id="callActionButton" data-id="'. $sessionUser->getSession_user_id() .'" data-modal-target="deleteModalUser" data-modal-toggle="deleteModalUser" class="inline-block w-8 h-8 bg-red-500 hover:bg-red-700 rounded" type="button">
+                    <button id="actionButton" data-id="'. $sessionUser->getSession_user_id() .'"data-action="deleteUser" class="inline-block w-8 h-8 bg-red-500 hover:bg-red-700 rounded" type="button">
                         <img class="p-1" src="/assets/images/trashcan.svg" alt="Delete">
                     </button>
                 </td>
